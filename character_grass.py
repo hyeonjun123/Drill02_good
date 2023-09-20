a699120da03362ef9167ef753b1350712dab670e
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-09-20 17:46:04 +0900

    화면에 장면과 캐릭터를 그렸다

--- character_grass.py
+++ character_grass.py
@@ -9,6 +9,10 @@ character = load_image('character.png')
 
 def run_circle():
     print('circle')
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
     pass
 
 def run_rectangle():
