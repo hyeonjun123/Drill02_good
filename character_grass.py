98b2027f0d04051bb0990bcfd60f03ec9764255d
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-09-20 18:13:27 +0900

    화면 렌더링 함수를 만들었다.

--- character_grass.py
+++ character_grass.py
@@ -7,6 +7,13 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
+def render_all(x,y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
+
+
 def run_circle():
     print('circle')
 
@@ -15,12 +22,9 @@ def run_circle():
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(deg/ 360 * 2 * math.pi)
         y = cy + r * math.sin(deg/ 360 * 2 * math.pi)
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.01)
-
+        render_all(x,y)
         
+
 
 
 def run_rectangle():
@@ -28,16 +32,14 @@ def run_rectangle():
     
     #bottom line
     for x in range(50, 750+1, 5):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)
-        
-    pass
+        render_all(x,90)
 
+            
+    #for x in range(750, 50, -5):
+    
 
 while True:
-#    run_circle()
+    run_circle()
     run_rectangle()
     #break
 
